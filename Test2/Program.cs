ab6d099569de88b7e5d0bc128432fea9fa067085
taufikprabowo1121 <solidiercyber@gmail.com>
2017-08-23 17:00:33 +0700

    Menjadida

---
+++ Test2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2
+{
+    class Program
+    {
+        void swapByValue(int x, int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+        void swapByReference(ref int x, ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+        static void Main(string[] args)
+        {
+            int a = 1; int b = 5;
+            Program p = new Program();
+
+            Console.WriteLine("Nilai x: " + a);
+            Console.WriteLine("Nilai x: " + b);
+
+            p.swapByValue(a, b);
+
+            Console.WriteLine("nilai x by val setelah swap :" + a);
+            Console.WriteLine("nilai x by val setelah swap :" + b);
+
+            p.swapByReference(ref a, ref b);
+
+            Console.WriteLine("nilai x by ref setelah swap : " + a);
+            Console.WriteLine("nilai x by ref stelah swap : " + b);
+            Console.ReadKey();
+        }
+    }
+}
